2bcc3b9bd72567d047558a75309d8c74b7df09a3
Leon-dre <98967558+Leon-dre@users.noreply.github.com>
2023-07-07 20:15:05 -0500

    Added some test output

---
+++ FinalProject/src/Models/Employee.java
@@ -0,0 +1,31 @@
+package Models;
+
+public class Employee extends User{
+
+    //attributes
+    private String jobTitle;
+
+    //default constructor
+    public Employee(){
+        super();
+        this.jobTitle = " default title";
+    }
+    //primary constructor
+    public Employee(int userID, String firstName, String lastName, String password, String jobTitle) {
+        super(userID, firstName, lastName, password);
+        this.jobTitle = jobTitle;
+    }
+    //copy constructor
+    public Employee(Employee emp){
+        
+        this.jobTitle = emp.getJobTitle();
+    }
+
+    //getters and setters
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
