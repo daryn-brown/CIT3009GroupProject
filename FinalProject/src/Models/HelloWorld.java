e04965bf135d2f196e4babb5e8a8ab402ef820ed
Leon-dre <98967558+Leon-dre@users.noreply.github.com>
2023-07-07 19:34:12 -0500

    Created Customer class

--- FinalProject/src/Models/HelloWorld.java
+++ FinalProject/src/Models/HelloWorld.java
@@ -3,9 +3,9 @@ package Models;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 			System.out.println("This a work now?");
+			System.out.println("This is working quite nicely");
 	}
 
 }
