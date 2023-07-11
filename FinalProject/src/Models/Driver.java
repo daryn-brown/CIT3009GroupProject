d6ee2b3db4a7bc3325aea321ebbda3eff2ae9a5c
Leon-dre <98967558+Leon-dre@users.noreply.github.com>
2023-07-10 20:57:04 -0500

    Refractor [hello world] to driver

---
+++ FinalProject/src/Models/Driver.java
@@ -0,0 +1,17 @@
+package Models;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+			System.out.println("This a work now?");
+			System.out.println("This is working quite nicely");
+
+			Customer customer = new Customer();
+			
+			customer.toString();
+
+			System.out.println("This is working");
+	}
+
+}
